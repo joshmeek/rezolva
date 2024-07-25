f9604bc05bdf6f37eb4973863ad8a135f23027c2
Josh Meek <joshmeek18@gmail.com>
2024-07-25 09:32:05 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="rezolva",
-    version="0.1.1",
+    version="0.2.0",
     author="Josh Meek",
     author_email="mail@josh.dev",
     description="Entity resolution for everyone. Minimal. No dependencies.",
